d129c953c5fd146e8dfc4b7322f4826b35e0e8ca
Harishkent <112664744+Harishkent@users.noreply.github.com>
2022-09-11 19:39:22 -0400

    Add files via upload

---
+++ ML Assigment-1.R
@@ -0,0 +1,5 @@
+head(Sales) # to get data of first 5 coloums
+tail(Sales) # to get data of last 5 coloums
+summary(Sales) # to get median,mean, and max values
+attach(Sales) # to display the variables.
+plot(Radio,Newspaper,main = "scattlerpolot") # to represent the scattlerplot by using plot command.
